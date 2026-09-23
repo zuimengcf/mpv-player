7f18c8a9f15e4bdd54cbd0aee16bc65fd28cf10e
zuimengcf <zuimengcf@users.noreply.github.com>
2026-09-23 10:25:09 +0000

    Revert "Limit ControlTab height to match other MoreSheet tabs"
    This reverts commit 28edc5f8c6ee89f6bbe795f582c1e332f5998509.

--- app/src/main/kotlin/xyz/mpv/rex/ui/player/controls/components/sheets/MoreSheet.kt
+++ app/src/main/kotlin/xyz/mpv/rex/ui/player/controls/components/sheets/MoreSheet.kt
@@ -575,7 +575,6 @@ fun ControlsTab(
       modifier = Modifier
           .fillMaxWidth()
           .padding(horizontal = MaterialTheme.spacing.medium)
-          .heightIn(max = 350.dp)
           .verticalScroll(rememberScrollState())
   ) {
       Text(
